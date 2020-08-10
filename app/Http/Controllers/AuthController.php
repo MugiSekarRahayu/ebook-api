59efe04e1ab5be1050487e645f4aed812d1489b8
MugiSekarRahayu <mugisekar.rahayu17@gmail.com>
2020-08-10 08:20:49 +0700

    add endpoint me

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //
+    public function me(){
+
+        return nl2br("nis : 3103118092 \n
+            nama : Mugi Sekar Rahayu \n
+            gender : Female \n
+            phone : 082136571083 \n
+            class : XII RPL 3");
+            
+        
+    }
+}
